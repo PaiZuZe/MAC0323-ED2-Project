3052eb4a9caa4bb21ecec1a8aabc2f2693d806cf
PaiZuZe <vitinho_cga@hotmail.com>
2018-03-27 14:54:05 -0300

    Adds names.

--- Projeto1/buffer.c
+++ Projeto1/buffer.c
@@ -1,3 +1,6 @@
+// Guilherme Costa Vieira               Nº USP: 9790930
+// Gabriel Kazuyuki Isomura             Nº USP: 9793673
+// Victor Chiaradia Gramuglia Araujo    Nº USP: 9793756
 #include "buffer.h"
 
 Buffer *buffer_resize(Buffer *B) {
